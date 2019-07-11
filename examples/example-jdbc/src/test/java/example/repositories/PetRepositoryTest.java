44db3aee4d4992274b54c286c576c89f62ed937d
graemerocher <graeme.rocher@gmail.com>
2019-07-11 17:15:25 +0200

    Add query test

--- examples/example-jdbc/src/test/java/example/repositories/PetRepositoryTest.java
+++ examples/example-jdbc/src/test/java/example/repositories/PetRepositoryTest.java
@@ -1,8 +1,14 @@
 package example.repositories;
 
 import example.domain.Pet;
+import io.micronaut.context.BeanContext;
+import io.micronaut.data.annotation.Query;
+import io.micronaut.inject.BeanDefinition;
+import io.micronaut.inject.ExecutableMethod;
 import io.micronaut.test.annotation.MicronautTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -12,6 +18,19 @@ public class PetRepositoryTest {
 
     @Inject
     PetRepository petRepository;
+
+
+    @Inject
+    BeanContext beanContext;
+
+    @Test
+    void testQuery() {
+        String query = beanContext.getBeanDefinition(PetRepository.class)
+                .getRequiredMethod("findByName", String.class)
+                .getAnnotationMetadata().stringValue(Query.class).get();
+
+        System.out.println("query = " + query);
+    }
 
 
     @Test
