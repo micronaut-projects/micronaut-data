b3bf60bea2c25ae40787c5d685707536207615de
Graeme Rocher <graeme.rocher@gmail.com>
2019-08-12 17:19:22 +0200

    Include some data in spring test

--- benchmarks/benchmark-spring-data-jdbc/src/test/java/example/BookRepositoryTest.java
+++ benchmarks/benchmark-spring-data-jdbc/src/test/java/example/BookRepositoryTest.java
@@ -3,10 +3,13 @@ package example;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @Transactional
@@ -14,11 +17,23 @@ class BookRepositoryTest {
 
 	@Autowired BookRepository bookRepository;
 
+	@BeforeEach
+	void setup() {
+		this.bookRepository.saveAll(Arrays.asList(
+				new Book("The Stand", 1000),
+				new Book("The Shining", 600),
+				new Book("The Power of the Dog", 500),
+				new Book("The Border", 700),
+				new Book("Along Came a Spider", 300),
+				new Book("Pet Cemetery", 400),
+				new Book("A Game of Thrones", 900),
+				new Book("A Clash of Kings", 1100)
+		));
+	}
 	@Test
 	void bookCount() {
-		bookRepository.findByTitle("Title");
-		bookRepository.findByTitle("Title");
-		Assertions.assertEquals(0, bookRepository.count());
+		bookRepository.findByTitle("The Stand");
+		Assertions.assertEquals(8, bookRepository.count());
 
 		Book book = bookRepository.save(new Book("Title", 42));
 
