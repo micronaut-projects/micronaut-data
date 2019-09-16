15f19b246b44db82308220477c3d14389b70d2d9
Graeme Rocher <graeme.rocher@gmail.com>
2019-09-16 08:16:11 -0700

    Fix example

--- examples/example-jdbc/src/main/java/example/repositories/PetRepository.java
+++ examples/example-jdbc/src/main/java/example/repositories/PetRepository.java
@@ -7,25 +7,16 @@ import java.util.UUID;
 import example.domain.NameDTO;
 import example.domain.Pet;
 import io.micronaut.data.annotation.Join;
-import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.Pageable;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.PageableRepository;
-import io.micronaut.spring.tx.annotation.Transactional;
 
-@JdbcRepository(dialect = Dialect.ORACLE)
-@Repository("other")
-public abstract class PetRepository implements PageableRepository<Pet, UUID> {
+@JdbcRepository(dialect = Dialect.H2)
+public interface PetRepository extends PageableRepository<Pet, UUID> {
 
-    abstract List<NameDTO> list(Pageable pageable);
+    List<NameDTO> list(Pageable pageable);
 
-    @Join("owner")
-    abstract Optional<Pet> findByName(String name);
-
-    abstract Optional<Pet> find(String name);
-
-    public Pet findPet(String name) {
-        return find(name).orElse(null);
-    }
+    @Join(value = "owner", type = Join.Type.LEFT)
+    Optional<Pet> findByName(String name);
 }
