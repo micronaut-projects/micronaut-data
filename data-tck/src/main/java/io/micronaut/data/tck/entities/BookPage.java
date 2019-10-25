ece8f0e0ec5726e57d1675a52a7c03ca39cc4794
graemerocher <graeme.rocher@gmail.com>
2019-10-25 11:52:25 +0200

    remove id annotations

--- data-tck/src/main/java/io/micronaut/data/tck/entities/BookPage.java
+++ data-tck/src/main/java/io/micronaut/data/tck/entities/BookPage.java
@@ -1,14 +1,10 @@
 package io.micronaut.data.tck.entities;
 
-import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 
 @MappedEntity
 public class BookPage {
-    @Id
     private Book book;
-
-    @Id
     private Page page;
 
     public Book getBook() {
