ec79a5c74b3b0ea107746681e8401c587b4d34a0
graemerocher <graeme.rocher@gmail.com>
2019-10-25 11:04:19 +0200

    Modify test for unimplemented cascading behaviour

--- data-jdbc/src/test/groovy/io/micronaut/data/jdbc/h2/H2UnidirectionalToManyJoinSpec.groovy
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/h2/H2UnidirectionalToManyJoinSpec.groovy
@@ -4,6 +4,7 @@ import io.micronaut.context.annotation.Property
 import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.tck.entities.Book
+import io.micronaut.data.tck.entities.Shelf
 import io.micronaut.data.tck.repositories.BookPageRepository
 import io.micronaut.data.tck.repositories.PageRepository
 import io.micronaut.data.tck.repositories.ShelfBookRepository
@@ -32,20 +33,25 @@ class H2UnidirectionalToManyJoinSpec extends Specification {
 
     void "test unidirectional join"() {
         given:
-        def shelf = shelfRepository.save("Some Shelf")
-        def b1 = bookRepository.save(new Book(title: "The Stand", totalPages: 1000))
-        def b2 = bookRepository.save(new Book(title: "The Shining", totalPages: 600))
+        Shelf shelf = new Shelf(shelfName: "Some Shelf")
+        shelf.books.add(new Book(title: "The Stand", totalPages: 1000))
+        shelf.books.add(new Book(title: "The Shining", totalPages: 600))
 
-        shelfBookRepository.save(shelf, b1)
-        shelfBookRepository.save(shelf, b2)
+        when:
+        shelf = shelfRepository.save(shelf)
+
+        then:
+        shelf.books.every { it.id != null }
+
+        when:
         def p10 = pageRepository.save(10)
         def p20 = pageRepository.save(20)
-        bookPageRepository.save(b1, p10)
-        bookPageRepository.save(b2, p20)
+        bookPageRepository.save(shelf.books[0], p10)
+        bookPageRepository.save(shelf.books[0], p20)
 
         shelf = shelfRepository.findById(shelf.id).orElse(null)
 
-        expect:
+        then:
         shelf != null
         shelf.shelfName == 'Some Shelf'
         // left join causes single result since each
