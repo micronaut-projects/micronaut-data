35455d95cc65fd694c8e49ee530c91ca2a795de9
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-21 10:03:59 +0200

    refactor: delete unecessary @TypeDef
    POJO Quantity is already annoated with @TypeDef

--- doc-examples/jdbc-example-groovy/src/main/groovy/example/Sale.groovy
+++ doc-examples/jdbc-example-groovy/src/main/groovy/example/Sale.groovy
@@ -1,8 +1,5 @@
 package example
 
-import io.micronaut.data.annotation.TypeDef
-import io.micronaut.data.model.DataType
-
 import javax.persistence.*
 
 @Entity
@@ -10,7 +7,7 @@ class Sale {
 
     @ManyToOne
     final Product product
-    @TypeDef(type = DataType.INTEGER)
+
     final Quantity quantity
 
     @Id
