9516cf74fca3b337e78b09ea7763c874fefac2f4
micronaut-build <micronaut-build@users.noreply.github.com>
2020-05-28 14:47:59 +0000

    Update common files

--- config/spotless.license.java
+++ config/spotless.license.java
@@ -1,5 +1,5 @@
 /*
- * Copyright $YEAR original authors
+ * Copyright 2017-$YEAR original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
