7d7a57ee0e126c4840d3a1e0182a6bab0f4e3a3b
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-01 15:21:46 +0200

    test: move stream to transactional MicronautTest
    Leave the rest of the DTO specification as a non transactional micronaut test

---
+++ data-hibernate-jpa/src/test/groovy/io/micronaut/data/hibernate/DtoStreamSpec.groovy
@@ -0,0 +1,40 @@
+package io.micronaut.data.hibernate
+
+import io.micronaut.context.annotation.Property
+import io.micronaut.data.tck.entities.BookDto
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Shared
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest(rollback = false, transactional = true, packages = "io.micronaut.data.tck.entities")
+@Property(name = "datasources.default.name", value = "mydb")
+@Property(name = 'jpa.default.properties.hibernate.hbm2ddl.auto', value = 'create-drop')
+class DtoStreamSpec extends Specification {
+
+    @Inject
+    @Shared
+    BookRepository bookRepository
+
+    @Inject
+    @Shared
+    BookDtoRepository bookDtoRepository
+
+    def setup() {
+        bookRepository.setupData()
+    }
+
+    def cleanup() {
+        bookRepository.deleteAll()
+    }
+
+    void "you can return a Stream of DTOs from a repository"() {
+        when: "Stream is used"
+        def dto = bookDtoRepository.findStream("The Stand").findFirst().get()
+
+        then: "The result is correct"
+        dto instanceof BookDto
+        dto.title == "The Stand"
+    }
+}
