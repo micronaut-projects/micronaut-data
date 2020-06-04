be1444c2ebadab2b53a6084f87e72a047b45ddff
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-04 08:05:05 +0200

    fix issue with multi-joins (#567)
    * refactor: extract method, build join query snippet
    
    * fix: multiple join query repeated join
    
    Close https://github.com/micronaut-projects/micronaut-data/issues/457

---
+++ data-jdbc/src/test/java/io/micronaut/data/jdbc/h2/ChallengeRepository.java
@@ -0,0 +1,22 @@
+package io.micronaut.data.jdbc.h2;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import io.micronaut.data.annotation.Join;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.tck.entities.Challenge;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@JdbcRepository(dialect = Dialect.H2)
+public interface ChallengeRepository extends CrudRepository<Challenge, Long> {
+
+    @Override
+    @Join(value = "authentication")
+    @Join(value = "authentication.device")
+    @Join(value = "authentication.device.user")
+    @NonNull
+    Optional<Challenge> findById(@NonNull @NotNull Long id);
+}
