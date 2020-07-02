0777cd8a7de331fe65f34ec272d5892a5750fd5e
Christian Wansart <cwansart@users.noreply.github.com>
2020-07-02 10:57:32 +0000

    Fix BookRepository example and indentation of BookRepositorySpec (#630)
    * Add tag start repository
    
    * Fix indentation of metadata example
    
    * Reformat BookRepositorySpec
    
    * Remove unnecessary newlines

--- doc-examples/example-java/src/test/java/example/BookRepositorySpec.java
+++ doc-examples/example-java/src/test/java/example/BookRepositorySpec.java
@@ -23,120 +23,100 @@ import java.util.List;
 class BookRepositorySpec {
 
     // tag::inject[]
-	@Inject BookRepository bookRepository;
+    @Inject
+    BookRepository bookRepository;
     // end::inject[]
 
-	@Inject AbstractBookRepository abstractBookRepository;
+    @Inject
+    AbstractBookRepository abstractBookRepository;
 
-	// tag::metadata[]
-	@Inject
-	BeanContext beanContext;
+    // tag::metadata[]
+    @Inject
+    BeanContext beanContext;
 
-	@Test
-	void testAnnotationMetadata() {
-		String query = beanContext.getBeanDefinition(BookRepository.class) // <1>
-								.getRequiredMethod("find", String.class) // <2>
-							    .getAnnotationMetadata()
-								.stringValue(Query.class) // <3>
-								.orElse(null);
+    @Test
+    void testAnnotationMetadata() {
+        String query = beanContext.getBeanDefinition(BookRepository.class) // <1>
+                .getRequiredMethod("find", String.class) // <2>
+                .getAnnotationMetadata().stringValue(Query.class) // <3>
+                .orElse(null);
 
-		assertEquals( // <4>
-				"SELECT book_ FROM example.Book AS book_ WHERE (book_.title = :p1)",
-				query
-		);
+        assertEquals( // <4>
+                "SELECT book_ FROM example.Book AS book_ WHERE (book_.title = :p1)", query);
 
-	}
-	// end::metadata[]
+    }
+    // end::metadata[]
 
-	@Test
-	void testCrud() {
-		assertNotNull(bookRepository);
+    @Test
+    void testCrud() {
+        assertNotNull(bookRepository);
 
-		// Create: Save a new book
+        // Create: Save a new book
         // tag::save[]
-		Book book = new Book();
-		book.setTitle("The Stand");
-		book.setPages(1000);
-		bookRepository.save(book);
+        Book book = new Book();
+        book.setTitle("The Stand");
+        book.setPages(1000);
+        bookRepository.save(book);
         // end::save[]
-		Long id = book.getId();
-		assertNotNull(id);
+        Long id = book.getId();
+        assertNotNull(id);
 
-		// Read: Read a book from the database
+        // Read: Read a book from the database
         // tag::read[]
-		book = bookRepository.findById(id).orElse(null);
+        book = bookRepository.findById(id).orElse(null);
         // end::read[]
-		assertNotNull(book);
-		assertEquals("The Stand", book.getTitle());
+        assertNotNull(book);
+        assertEquals("The Stand", book.getTitle());
 
-		// Check the count
-		assertEquals(1, bookRepository.count());
-		assertTrue(bookRepository.findAll().iterator().hasNext());
-		
-		// Update: Update the book and save it again
+        // Check the count
+        assertEquals(1, bookRepository.count());
+        assertTrue(bookRepository.findAll().iterator().hasNext());
+
+        // Update: Update the book and save it again
         // tag::update[]
-		book.setTitle("Changed");
-		bookRepository.save(book);
+        book.setTitle("Changed");
+        bookRepository.save(book);
         // end::update[]
-		book = bookRepository.findById(id).orElse(null);
-		assertEquals("Changed", book.getTitle());
+        book = bookRepository.findById(id).orElse(null);
+        assertEquals("Changed", book.getTitle());
 
-		// Delete: Delete the book
+        // Delete: Delete the book
         // tag::delete[]
-		bookRepository.deleteById(id);
+        bookRepository.deleteById(id);
         // end::delete[]
-		assertEquals(0, bookRepository.count());
-	}
+        assertEquals(0, bookRepository.count());
+    }
 
-	@Test
-	void testPageable() {
-		// tag::saveall[]
-		bookRepository.saveAll(Arrays.asList(
-				new Book("The Stand", 1000),
-				new Book("The Shining", 600),
-				new Book("The Power of the Dog", 500),
-				new Book("The Border", 700),
-				new Book("Along Came a Spider", 300),
-				new Book("Pet Cemetery", 400),
-				new Book("A Game of Thrones", 900),
-				new Book("A Clash of Kings", 1100)
-		));
-		// end::saveall[]
+    @Test
+    void testPageable() {
+        // tag::saveall[]
+        bookRepository.saveAll(Arrays.asList(new Book("The Stand", 1000), new Book("The Shining", 600),
+                new Book("The Power of the Dog", 500), new Book("The Border", 700),
+                new Book("Along Came a Spider", 300), new Book("Pet Cemetery", 400), new Book("A Game of Thrones", 900),
+                new Book("A Clash of Kings", 1100)));
+        // end::saveall[]
 
-		// tag::pageable[]
-		Slice<Book> slice = bookRepository.list(Pageable.from(0, 3));
-		List<Book> resultList =
-				bookRepository.findByPagesGreaterThan(500, Pageable.from(0, 3));
-		Page<Book> page = bookRepository.findByTitleLike("The%", Pageable.from(0, 3));
-		// end::pageable[]
+        // tag::pageable[]
+        Slice<Book> slice = bookRepository.list(Pageable.from(0, 3));
+        List<Book> resultList = bookRepository.findByPagesGreaterThan(500, Pageable.from(0, 3));
+        Page<Book> page = bookRepository.findByTitleLike("The%", Pageable.from(0, 3));
+        // end::pageable[]
 
-		assertEquals(
-				3,
-				slice.getNumberOfElements()
-		);
-		assertEquals(
-				3,
-				resultList.size()
-		);
-		assertEquals(
-				3,
-				page.getNumberOfElements()
-		);
-		assertEquals(
-				4,
-				page.getTotalSize()
-		);
+        assertEquals(3, slice.getNumberOfElements());
+        assertEquals(3, resultList.size());
+        assertEquals(3, page.getNumberOfElements());
+        assertEquals(4, page.getTotalSize());
 
-		List<Book> results = abstractBookRepository.findByTitle("The Shining");
+        List<Book> results = abstractBookRepository.findByTitle("The Shining");
 
-		assertEquals(1, results.size());
-	}
+        assertEquals(1, results.size());
+    }
 
-	@Test
-	void testDto() {
-		bookRepository.save(new Book("The Shining", 400));
-		BookDTO book = bookRepository.findOne("The Shining");
+    @Test
+    void testDto() {
+        bookRepository.save(new Book("The Shining", 400));
+        BookDTO book = bookRepository.findOne("The Shining");
 
-		assertEquals("The Shining", book.getTitle());
-	}
+        assertEquals("The Shining", book.getTitle());
+    }
 }
