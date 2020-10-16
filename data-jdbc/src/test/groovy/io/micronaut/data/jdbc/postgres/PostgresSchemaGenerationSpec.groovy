f7e39e47b5e16bc82f5564c115c65550483c0be0
graemerocher <graeme.rocher@gmail.com>
2020-10-16 11:21:29 +0200

    Fix test

--- data-jdbc/src/test/groovy/io/micronaut/data/jdbc/postgres/PostgresSchemaGenerationSpec.groovy
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/postgres/PostgresSchemaGenerationSpec.groovy
@@ -1,10 +1,16 @@
 package io.micronaut.data.jdbc.postgres
 
-
+import groovy.sql.Sql
+import io.micronaut.context.event.BeanCreatedEvent
+import io.micronaut.context.event.BeanCreatedEventListener
+import io.micronaut.core.order.Ordered
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
 import spock.lang.Specification
 
 import javax.inject.Inject
+import javax.inject.Singleton
+import javax.sql.DataSource
 
 @MicronautTest
 class PostgresSchemaGenerationSpec extends Specification implements PostgresTestPropertyProvider {
@@ -15,5 +21,23 @@ class PostgresSchemaGenerationSpec extends Specification implements PostgresTest
     void "test uuid generated value"() {
         expect:
         repository.count() == 0
+    }
+
+    // sets up UUID extension support for Postgres
+    @MockBean
+    @Singleton
+    static class ExtensionSetup implements BeanCreatedEventListener<DataSource>, Ordered {
+
+        @Override
+        DataSource onCreated(BeanCreatedEvent<DataSource> event) {
+            def dataSource = event.getBean()
+            new Sql(dataSource).execute('CREATE EXTENSION "uuid-ossp";')
+            return dataSource
+        }
+
+        @Override
+        int getOrder() {
+            return HIGHEST_PRECEDENCE
+        }
     }
 }
