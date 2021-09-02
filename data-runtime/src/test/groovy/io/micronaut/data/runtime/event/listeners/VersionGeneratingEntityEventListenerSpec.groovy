e37c328d3f6d2664fc01a68db05d7d098cbb9ec1
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-09-02 16:09:28 +0200

    Correct temporal version conversion (#1141)

---
+++ data-runtime/src/test/groovy/io/micronaut/data/runtime/event/listeners/VersionGeneratingEntityEventListenerSpec.groovy
@@ -0,0 +1,43 @@
+package io.micronaut.data.runtime.event.listeners
+
+import io.micronaut.data.annotation.AutoPopulated
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.Version
+import io.micronaut.data.model.runtime.RuntimePersistentEntity
+import io.micronaut.data.runtime.event.DefaultEntityEventContext
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+import java.time.Instant
+
+@MicronautTest
+class VersionGeneratingEntityEventListenerSpec extends Specification {
+
+    @Inject
+    VersionGeneratingEntityEventListener entityEventListener
+
+    void "test instant version set"() {
+        when:
+            def entity = new TheEntity()
+            def mockEvent = new DefaultEntityEventContext(RuntimePersistentEntity.of(TheEntity), entity)
+            entityEventListener.prePersist(mockEvent)
+        then:
+            entity.ver
+        when:
+            entityEventListener.preUpdate(mockEvent)
+        then:
+            entity.ver
+    }
+}
+
+@MappedEntity
+class TheEntity {
+    @Id
+    @AutoPopulated
+    UUID uuid
+
+    @Version
+    Instant ver
+}
