8fdd0725052b81275fcab212a6731c87b4869657
Denis Stepanov <denis.s.stepanov@oracle.com>
2021-10-20 10:58:33 +0200

    Disable `autoApply` `Converter` check (#1203)

---
+++ data-hibernate-jpa/src/test/java/io/micronaut/data/hibernate/CustomConverter.java
@@ -0,0 +1,22 @@
+package io.micronaut.data.hibernate;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CustomConverter implements AttributeConverter<A, B> {
+
+    @Override
+    public B convertToDatabaseColumn(A attribute) {
+        return null;
+    }
+
+    @Override
+    public A convertToEntityAttribute(B dbData) {
+        return null;
+    }
+
+}
+
+class A {}
+class B {}
