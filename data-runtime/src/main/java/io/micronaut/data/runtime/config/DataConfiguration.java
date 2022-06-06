32ebd45d85aa66c25dd16c7b4872246da5050bcb
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-06-06 14:11:01 -0500

    added startFromPageOne config value

--- data-runtime/src/main/java/io/micronaut/data/runtime/config/DataConfiguration.java
+++ data-runtime/src/main/java/io/micronaut/data/runtime/config/DataConfiguration.java
@@ -36,6 +36,7 @@ public class DataConfiguration implements DataSettings {
     public static class PageableConfiguration {
         public static final int    DEFAULT_MAX_PAGE_SIZE = 100;
         public static final boolean DEFAULT_SORT_IGNORE_CASE = false;
+        public static final boolean DEFAULT_START_FROM_PAGE_ONE = false;
         public static final String DEFAULT_SORT_PARAMETER = "sort";
         public static final String DEFAULT_SIZE_PARAMETER = "size";
         public static final String DEFAULT_PAGE_PARAMETER = "page";
@@ -43,6 +44,7 @@ public class DataConfiguration implements DataSettings {
         private int maxPageSize = DEFAULT_MAX_PAGE_SIZE;
         private Integer defaultPageSize = null; // When is not specified the maxPageSize should be used
         private boolean sortIgnoreCase = DEFAULT_SORT_IGNORE_CASE;
+        private boolean startFromPageOne = DEFAULT_START_FROM_PAGE_ONE;
         private String sortParameterName = DEFAULT_SORT_PARAMETER;
         private String sizeParameterName = DEFAULT_SIZE_PARAMETER;
         private String pageParameterName = DEFAULT_PAGE_PARAMETER;
@@ -76,6 +78,21 @@ public class DataConfiguration implements DataSettings {
             if (StringUtils.isNotEmpty(sortDelimiter)) {
                 this.sortDelimiter = Pattern.compile(Pattern.quote(sortDelimiter));
             }
+        }
+
+        /**
+         * @return Whether the first page is 1
+         */
+        public boolean isStartFromPageOne() {
+            return startFromPageOne;
+        }
+
+        /**
+         * This parameter is used to shift the provided page number back one when this is true.
+         * @param startFromPageOne Whether the first page is 1
+         */
+        public void setStartFromPageOne(boolean startFromPageOne) {
+            this.startFromPageOne = startFromPageOne;
         }
 
         /**
