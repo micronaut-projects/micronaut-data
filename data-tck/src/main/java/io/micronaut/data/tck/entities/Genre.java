ac71540c09ac5ffb131d1db6f7c9a37ba13a71e2
radovanradic <radicr@gmail.com>
2022-08-17 12:32:10 +0200

    Reverted Genre entity change, no need to extend Generic

--- data-tck/src/main/java/io/micronaut/data/tck/entities/Genre.java
+++ data-tck/src/main/java/io/micronaut/data/tck/entities/Genre.java
@@ -1,11 +1,25 @@
 package io.micronaut.data.tck.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
-public class Genre extends GenericEntity<Long> {
+public class Genre {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     private String genreName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getGenreName() {
         return genreName;
