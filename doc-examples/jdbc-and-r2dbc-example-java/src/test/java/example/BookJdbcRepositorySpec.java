0601bb07f31c6922623dcc7114b50dad557fc724
Denis Stepanov <denis.s.stepanov@oracle.com>
2022-09-06 18:11:08 +0300

    Rollback individual transaction changes (#1723)

--- doc-examples/jdbc-and-r2dbc-example-java/src/test/java/example/BookJdbcRepositorySpec.java
+++ doc-examples/jdbc-and-r2dbc-example-java/src/test/java/example/BookJdbcRepositorySpec.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@MicronautTest
+@MicronautTest(transactional = false)
 class BookJdbcRepositorySpec {
 
 	@Inject BookJdbcRepository bookRepository;
