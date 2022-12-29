6f40b65641f8dca9514ac330d8544871506e80a4
radovanradic <radicr@gmail.com>
2022-12-29 09:06:15 +0100

    Fix map of objects handling in MongoDb (#1948)
    * Fix map of objects handling in MongoDb
    
    * Updated the test
    
    * Fix the test

--- data-document-tck/src/main/java/io/micronaut/data/document/tck/entities/Document.java
+++ data-document-tck/src/main/java/io/micronaut/data/document/tck/entities/Document.java
@@ -5,6 +5,7 @@ import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 
 import java.util.List;
+import java.util.Map;
 
 @MappedEntity
 public class Document {
@@ -16,6 +17,8 @@ public class Document {
     private String title;
 
     private List<String> tags;
+
+    private Map<String, Owner> owners;
 
     public String getId() {
         return id;
@@ -39,5 +42,13 @@ public class Document {
 
     public void setTags(List<String> tags) {
         this.tags = tags;
+    }
+
+    public Map<String, Owner> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(Map<String, Owner> owners) {
+        this.owners = owners;
     }
 }
