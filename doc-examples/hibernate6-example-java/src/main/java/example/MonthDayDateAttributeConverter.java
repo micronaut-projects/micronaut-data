92aa4024795bde936392246d1cf00b3d51822d3e
radovanradic <radicr@gmail.com>
2023-01-11 11:43:03 +0100

    Update docs, hibernate6 example

--- doc-examples/hibernate6-example-java/src/main/java/example/MonthDayDateAttributeConverter.java
+++ doc-examples/hibernate6-example-java/src/main/java/example/MonthDayDateAttributeConverter.java
@@ -1,7 +1,7 @@
 package example;
 
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 import java.time.LocalDate;
 import java.time.MonthDay;
 import java.sql.Date;
---
+++ doc-examples/hibernate6-example-java/src/test/java/example/BookRepositorySpec.java
@@ -0,0 +1,133 @@
+package example;
+
+import io.micronaut.context.BeanContext;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.model.Slice;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+@Property(name = "datasources.default.name", value = "mydb")
+@Property(name = "jpa.default.properties.hibernate.hbm2ddl.auto", value = "create-drop")
+class BookRepositorySpec {
+
+    // tag::inject[]
+    @Inject
+    BookRepository bookRepository;
+    // end::inject[]
+
+    @Inject
+    AbstractBookRepository abstractBookRepository;
+
+    // tag::metadata[]
+    @Inject
+    BeanContext beanContext;
+
+    @Test
+    void testAnnotationMetadata() {
+        String query = beanContext.getBeanDefinition(BookRepository.class) // <1>
+                .getRequiredMethod("find", String.class) // <2>
+                .getAnnotationMetadata().stringValue(Query.class) // <3>
+                .orElse(null);
+
+        assertEquals( // <4>
+                "SELECT book_ FROM example.Book AS book_ WHERE (book_.title = :p1)", query);
+
+    }
+    // end::metadata[]
+
+    @AfterEach
+    public void cleanup() {
+        bookRepository.deleteAll();
+    }
+
+    @Test
+    void testCrud() {
+        assertNotNull(bookRepository);
+
+        // Create: Save a new book
+        // tag::save[]
+        Book book = new Book();
+        book.setTitle("The Stand");
+        book.setPages(1000);
+        bookRepository.save(book);
+        // end::save[]
+        Long id = book.getId();
+        assertNotNull(id);
+
+        // Read: Read a book from the database
+        // tag::read[]
+        book = bookRepository.findById(id).orElse(null);
+        // end::read[]
+        assertNotNull(book);
+        assertEquals("The Stand", book.getTitle());
+
+        // Check the count
+        assertEquals(1, bookRepository.count());
+        assertTrue(bookRepository.findAll().iterator().hasNext());
+
+        // Update: Update the book and save it again
+        // tag::update[]
+        book.setTitle("Changed");
+        bookRepository.save(book);
+        // end::update[]
+        book = bookRepository.findById(id).orElse(null);
+        assertEquals("Changed", book.getTitle());
+
+        // Partially update book via executeUpdate
+        bookRepository.updatePages(id, 1200);
+        book = bookRepository.findById(id).orElse(null);
+        assertEquals(1200, book.getPages());
+
+        // Delete: Delete the book
+        // tag::delete[]
+        bookRepository.deleteById(id);
+        // end::delete[]
+        assertEquals(0, bookRepository.count());
+    }
+
+    @Test
+    void testPageable() {
+        // tag::saveall[]
+        bookRepository.saveAll(Arrays.asList(new Book("The Stand", 1000), new Book("The Shining", 600),
+                new Book("The Power of the Dog", 500), new Book("The Border", 700),
+                new Book("Along Came a Spider", 300), new Book("Pet Cemetery", 400), new Book("A Game of Thrones", 900),
+                new Book("A Clash of Kings", 1100)));
+        // end::saveall[]
+
+        // tag::pageable[]
+        Slice<Book> slice = bookRepository.list(Pageable.from(0, 3));
+        List<Book> resultList = bookRepository.findByPagesGreaterThan(500, Pageable.from(0, 3));
+        Page<Book> page = bookRepository.findByTitleLike("The%", Pageable.from(0, 3));
+        // end::pageable[]
+
+        assertEquals(3, slice.getNumberOfElements());
+        assertEquals(3, resultList.size());
+        assertEquals(3, page.getNumberOfElements());
+        assertEquals(4, page.getTotalSize());
+
+        List<Book> results = abstractBookRepository.findByTitle("The Shining");
+
+        assertEquals(1, results.size());
+    }
+
+    @Test
+    void testDto() {
+        bookRepository.save(new Book("The Shining", 400));
+        BookDTO book = bookRepository.findOne("The Shining");
+
+        assertEquals("The Shining", book.getTitle());
+    }
+}
