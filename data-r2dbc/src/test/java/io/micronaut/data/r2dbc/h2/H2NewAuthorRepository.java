8eefdf0c1aa8249cae61bf51978bea207ec83504
radovanradic <radicr@gmail.com>
2023-02-27 13:38:48 +0100

    Update test using different basic repo

--- data-r2dbc/src/test/java/io/micronaut/data/r2dbc/h2/H2NewAuthorRepository.java
+++ data-r2dbc/src/test/java/io/micronaut/data/r2dbc/h2/H2NewAuthorRepository.java
@@ -4,6 +4,7 @@ import io.micronaut.data.annotation.Join;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
 import io.micronaut.data.repository.PageableRepository;
+import io.micronaut.data.repository.reactive.ReactiveStreamsPageableRepository;
 
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
@@ -12,7 +13,7 @@ import java.util.Optional;
 @Join(value = "books", type = Join.Type.FETCH)
 @R2dbcRepository(dialect = Dialect.H2)
 @Transactional
-public interface H2NewAuthorRepository extends PageableRepository<NewAuthor, Long> {
+public interface H2NewAuthorRepository extends ReactiveStreamsPageableRepository<NewAuthor, Long> {
     NewAuthor save(@NotNull final String name);
 
 
