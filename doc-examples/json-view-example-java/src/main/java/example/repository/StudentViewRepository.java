65c024da57fb6ff195540a89653c306331cbf7a7
radovanradic <radicr@gmail.com>
2023-02-28 11:22:20 +0100

    Removed unneeded annotation

--- doc-examples/json-view-example-java/src/main/java/example/repository/StudentViewRepository.java
+++ doc-examples/json-view-example-java/src/main/java/example/repository/StudentViewRepository.java
@@ -6,12 +6,10 @@ import io.micronaut.data.annotation.Query;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.GenericRepository;
-import jakarta.inject.Singleton;
 
 import java.util.List;
 import java.util.Optional;
 
-@Singleton
 @JdbcRepository(dialect = Dialect.ORACLE)
 public interface StudentViewRepository extends GenericRepository<StudentView, Long> {
 
