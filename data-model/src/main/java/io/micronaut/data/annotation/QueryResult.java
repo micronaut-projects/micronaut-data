ac12797d5946db4a81bc873bbba97c6aede3d727
radovanradic <radicr@gmail.com>
2023-03-01 16:26:45 +0100

    QueryResult can be defined on class or method

--- data-model/src/main/java/io/micronaut/data/annotation/QueryResult.java
+++ data-model/src/main/java/io/micronaut/data/annotation/QueryResult.java
@@ -30,7 +30,7 @@ import java.lang.annotation.Target;
  * @since 4.0.0
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
 @Inherited
 public @interface QueryResult {
