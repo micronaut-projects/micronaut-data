3026c0c5799c3e96768bbaad84881119d1d1b273
Sergio del Amo <sergio.delamo@softamo.com>
2023-04-25 13:27:06 +0200

    build: Micronaut Framework to M2 (#2152)
    * Fix kotlin test dependencies
    
    * Fix Cosmos Serde according to changes in JacksonDecoder
    
    ---------
    
    Co-authored-by: radovanradic <radicr@gmail.com>

--- doc-examples/jdbc-example-kotlin/src/test/kotlin/example/ProjectConfig.kt
+++ doc-examples/jdbc-example-kotlin/src/test/kotlin/example/ProjectConfig.kt
@@ -1,9 +1,9 @@
 package example
 
 import io.kotest.core.config.AbstractProjectConfig
-import io.micronaut.test.extensions.kotest.MicronautKotestExtension
+import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 
 object ProjectConfig : AbstractProjectConfig() {
-    override fun listeners() = listOf(MicronautKotestExtension)
-    override fun extensions() = listOf(MicronautKotestExtension)
+    override fun listeners() = listOf(MicronautKotest5Extension)
+    override fun extensions() = listOf(MicronautKotest5Extension)
 }
