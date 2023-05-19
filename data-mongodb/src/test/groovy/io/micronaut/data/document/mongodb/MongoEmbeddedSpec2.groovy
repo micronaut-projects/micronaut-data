2d198aed03a5bfb59964505313a07a3ba5b384f6
radovanradic <radicr@gmail.com>
2023-05-19 18:12:02 +0200

    Revert indentation

--- data-mongodb/src/test/groovy/io/micronaut/data/document/mongodb/MongoEmbeddedSpec2.groovy
+++ data-mongodb/src/test/groovy/io/micronaut/data/document/mongodb/MongoEmbeddedSpec2.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package example // Keep example package for testing non micronaut package
+package example  // Keep example package for testing non micronaut package
 
 import io.micronaut.core.annotation.Nullable
 import io.micronaut.data.annotation.Embeddable
