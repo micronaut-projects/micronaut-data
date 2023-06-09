fdddc43c3f02657758ee34e504c3ba40b9855865
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-06-09 02:45:50 -0500

    Support opening reactive transaction in tests (#2261)
    * Support opening reactive transaction in tests
    
    ---------
    
    Co-authored-by: radovanradic <radicr@gmail.com>

--- data-r2dbc/src/test/java/io/micronaut/data/r2dbc/h2/H2BookRepository.java
+++ data-r2dbc/src/test/java/io/micronaut/data/r2dbc/h2/H2BookRepository.java
@@ -2,11 +2,18 @@ package io.micronaut.data.r2dbc.h2;
 
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
+import io.micronaut.data.tck.entities.Book;
 import io.micronaut.data.tck.repositories.BookRepository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @R2dbcRepository(dialect = Dialect.H2)
 public abstract class H2BookRepository extends BookRepository {
     public H2BookRepository(H2AuthorRepository authorRepository) {
         super(authorRepository);
     }
+
+    @Transactional(Transactional.TxType.MANDATORY)
+    public abstract Optional<Book> queryById(Long id);
 }
