8de9514b671fe6548f8160c226235c91d18c12a2
radovanradic <radicr@gmail.com>
2023-10-12 15:14:22 +0200

    Potential option to support UPDATE RETURNING for JPA/Hibernate (#2559)

--- data-hibernate-jpa/src/test/java/io/micronaut/data/hibernate/UserWithWhereRepository.java
+++ data-hibernate-jpa/src/test/java/io/micronaut/data/hibernate/UserWithWhereRepository.java
@@ -1,6 +1,7 @@
 package io.micronaut.data.hibernate;
 
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.hibernate.entities.UserWithWhere;
 import io.micronaut.data.model.Sort;
@@ -13,4 +14,12 @@ import java.util.UUID;
 public interface UserWithWhereRepository extends CrudRepository<UserWithWhere, UUID> {
 
     List<UserWithWhere> findAllByIdInList(List<UUID> ids, @NonNull Sort sort);
+
+    @Query(value = "UPDATE users SET email = :email, deleted = :deleted WHERE id = :id RETURNING *", nativeQuery = true)
+    UserWithWhere updateReturningCustom(String email, boolean deleted, UUID id);
+
+    @Query(value = "UPDATE users SET email = :email WHERE id = :id RETURNING email", nativeQuery = true)
+    String updateAndReturnEmail(String email, UUID id);
+
+    void updateEmailById(UUID id, String email);
 }
