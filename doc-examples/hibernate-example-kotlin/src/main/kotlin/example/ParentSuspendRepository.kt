a505a30dd93d43074b30f5ad5c678d9e53c0a215
Kareem Elzayat <kareem.elzayat@uberall.com>
2024-02-06 09:32:00 +0100

    Add RepositoryConfiguration with JpaQueryBuilder

--- doc-examples/hibernate-example-kotlin/src/main/kotlin/example/ParentSuspendRepository.kt
+++ doc-examples/hibernate-example-kotlin/src/main/kotlin/example/ParentSuspendRepository.kt
@@ -3,12 +3,15 @@ package example
 import io.micronaut.core.annotation.NonNull
 import io.micronaut.data.annotation.Join
 import io.micronaut.data.annotation.Repository
+import io.micronaut.data.annotation.RepositoryConfiguration
+import io.micronaut.data.model.query.builder.jpa.JpaQueryBuilder
 import io.micronaut.data.repository.GenericRepository
 import io.micronaut.data.repository.jpa.kotlin.CoroutineJpaSpecificationExecutor
-import java.util.*
 import jakarta.transaction.Transactional
+import java.util.*
 
 @Repository
+@RepositoryConfiguration(queryBuilder = JpaQueryBuilder::class) // it was necessary to define a QueryBuilder for the findAll(): Flow scenario
 interface ParentSuspendRepository : GenericRepository<Parent, Int>, CoroutineJpaSpecificationExecutor<Parent> {
 
     @Join(value = "children", type = Join.Type.FETCH)
