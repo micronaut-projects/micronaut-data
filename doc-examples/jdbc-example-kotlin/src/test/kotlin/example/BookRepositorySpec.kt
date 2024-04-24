0ccf54a09b8cabf5b5963bd83c8dda7570b87c0c
Andriy Dmytruk <andriy.dmytruk@oracle.com>
2024-04-24 14:16:50 -0400

    Add documentation for cursored pageable

--- doc-examples/jdbc-example-kotlin/src/test/kotlin/example/BookRepositorySpec.kt
+++ doc-examples/jdbc-example-kotlin/src/test/kotlin/example/BookRepositorySpec.kt
@@ -2,14 +2,15 @@ package example
 
 import io.micronaut.context.BeanContext
 import io.micronaut.data.annotation.Query
+import io.micronaut.data.model.CursoredPageable
 import io.micronaut.data.model.Pageable
+import io.micronaut.data.model.Sort
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
-import jakarta.inject.Inject
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 
 @MicronautTest
 class BookRepositorySpec {
@@ -127,6 +128,49 @@ class BookRepositorySpec {
         val results = abstractBookRepository.findByTitle("The Shining")
 
         assertEquals(1, results.size)
+    }
+
+    @Test
+    fun testCursoredPageable() {
+        bookRepository.saveAll(
+            Arrays.asList(
+                Book(0, "The Stand", 1000),
+                Book(0, "The Shining", 600),
+                Book(0, "The Power of the Dog", 500),
+                Book(0, "The Border", 700),
+                Book(0, "Along Came a Spider", 300),
+                Book(0, "Pet Cemetery", 400),
+                Book(0, "A Game of Thrones", 900),
+                Book(0, "A Clash of Kings", 1100)
+            )
+        )
+
+        // tag::cursored-pageable[]
+        val page =  // <1>
+            bookRepository.find(CursoredPageable.from(5, Sort.of(Sort.Order.asc("title"))))
+        val page2 = bookRepository.find(page.nextPageable()) // <2>
+        val pageByPagesBetween =  // <3>
+            bookRepository.findByPagesBetween(400, 700, Pageable.from(0, 3))
+        val pageByTitleStarts =  // <4>
+            bookRepository.findByTitleStartingWith("The", CursoredPageable.from(3, Sort.unsorted()))
+        // end::cursored-pageable[]
+
+        assertEquals(
+            5,
+            page.numberOfElements
+        )
+        assertEquals(
+            3,
+            page2.numberOfElements
+        )
+        assertEquals(
+            3,
+            pageByPagesBetween.numberOfElements
+        )
+        assertEquals(
+            3,
+            pageByTitleStarts.numberOfElements
+        )
     }
 
     @Test
