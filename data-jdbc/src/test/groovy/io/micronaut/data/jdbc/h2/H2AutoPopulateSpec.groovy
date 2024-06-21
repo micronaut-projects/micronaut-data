b64b3278154bed5117ba530223275552b1b1dde9
radovanradic <radicr@gmail.com>
2024-06-21 12:38:53 +0200

    Auto populated not supported for embedded/nested fields.

---
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/h2/H2AutoPopulateSpec.groovy
@@ -0,0 +1,158 @@
+package io.micronaut.data.jdbc.h2
+
+import io.micronaut.data.annotation.AutoPopulated
+import io.micronaut.data.annotation.DateCreated
+import io.micronaut.data.annotation.DateUpdated
+import io.micronaut.data.annotation.Embeddable
+import io.micronaut.data.annotation.EmbeddedId
+import io.micronaut.data.annotation.MappedEntity
+import io.micronaut.data.annotation.MappedProperty
+import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.repository.CrudRepository
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import jakarta.persistence.Embedded
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.time.Instant
+
+@MicronautTest
+@H2DBProperties
+class H2AutoPopulateSpec extends Specification {
+
+    @Inject
+    @Shared
+    H2ExampleRepository exampleRepository
+
+    void "test save and read entity"() {
+        when:"an entity is saved"
+        def exampleId = new ExampleId(UUID.randomUUID(), "data1")
+        def exampleEntity = new ExampleEntity(exampleId, "Test 1")
+        exampleRepository.save(exampleEntity)
+
+        then:
+        exampleEntity.id.otherProp == "data1"
+        exampleEntity.id.uuid
+
+        when:"An entity is retrieved by ID"
+        exampleEntity = exampleRepository.findById(exampleEntity.id).orElse(null)
+
+        then:"The entity is correct"
+        exampleEntity.id.otherProp == "data1"
+        exampleEntity.id.uuid
+        exampleEntity.name == "Test 1"
+        exampleEntity.audit
+        exampleEntity.audit.dateCreated
+        exampleEntity.audit.dateUpdated
+        exampleEntity.audit.recordId
+    }
+}
+
+@MappedEntity
+class ExampleEntity {
+    @MappedProperty("")
+    @EmbeddedId
+    private ExampleId id
+    @MappedProperty("")
+    @Embedded
+    private Audit audit
+    private String name
+
+    ExampleEntity(ExampleId id, String name) {
+        this.id = id
+        this.name = name
+        this.audit = new Audit()
+    }
+
+    ExampleId getId() {
+        return id
+    }
+
+    void setId(ExampleId id) {
+        this.id = id
+    }
+
+    Audit getAudit() {
+        return audit
+    }
+
+    void setAudit(Audit audit) {
+        this.audit = audit
+    }
+
+    String getName() {
+        return name
+    }
+
+    void setName(String name) {
+        this.name = name
+    }
+}
+
+@Embeddable
+class ExampleId {
+    @AutoPopulated
+    private UUID uuid
+    private String otherProp
+
+    ExampleId(UUID uuid, String otherProp) {
+        this.uuid = uuid
+        this.otherProp = otherProp
+    }
+
+    UUID getUuid() {
+        return uuid
+    }
+
+    void setUuid(UUID uuid) {
+        this.uuid = uuid
+    }
+
+    String getOtherProp() {
+        return otherProp
+    }
+
+    void setOtherProp(String otherProp) {
+        this.otherProp = otherProp
+    }
+}
+
+@Embeddable
+class Audit {
+    @DateCreated
+    private Instant dateCreated
+    @DateUpdated
+    private Instant dateUpdated
+    @AutoPopulated
+    private UUID recordId
+
+    Instant getDateCreated() {
+        return dateCreated
+    }
+
+    void setDateCreated(Instant dateCreated) {
+        this.dateCreated = dateCreated
+    }
+
+    Instant getDateUpdated() {
+        return dateUpdated
+    }
+
+    void setDateUpdated(Instant dateUpdated) {
+        this.dateUpdated = dateUpdated
+    }
+
+    UUID getRecordId() {
+        return recordId
+    }
+
+    void setRecordId(UUID recordId) {
+        this.recordId = recordId
+    }
+}
+
+@JdbcRepository(dialect = Dialect.H2)
+public interface H2ExampleRepository extends CrudRepository<ExampleEntity, ExampleId> {
+}
