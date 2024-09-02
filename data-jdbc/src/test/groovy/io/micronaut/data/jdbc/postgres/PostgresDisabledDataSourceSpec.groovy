7a635b02812fcb2aa9c3673c9438a4d57031542d
Radovan Radic <radicr@gmail.com>
2024-09-02 11:19:03 +0200

    Skip schema generation when datasource is disabled (#3066)

---
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/postgres/PostgresDisabledDataSourceSpec.groovy
@@ -0,0 +1,40 @@
+package io.micronaut.data.jdbc.postgres
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.exceptions.NoSuchBeanException
+import io.micronaut.data.jdbc.config.DataJdbcConfiguration
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Shared
+import spock.lang.Specification
+
+import javax.sql.DataSource
+
+@MicronautTest(transactional = false)
+class PostgresDisabledDataSourceSpec extends Specification implements PostgresTestPropertyProvider {
+
+    @Inject
+    @Shared
+    ApplicationContext applicationContext
+
+
+    void 'test disabled data source'() {
+        when:
+        applicationContext.getBean(DataSource)
+        then:
+        thrown(NoSuchBeanException)
+        when:
+        def dataJdbcConfiguration = applicationContext.getBean(DataJdbcConfiguration)
+        then:
+        dataJdbcConfiguration
+        !dataJdbcConfiguration.enabled
+    }
+
+    @Override
+    boolean dataSourceEnabled(String dataSourceName) {
+        if (dataSourceName == 'default') {
+            return false
+        }
+        return PostgresTestPropertyProvider.class.dataSourceEnabled(dataSourceName)
+    }
+}
