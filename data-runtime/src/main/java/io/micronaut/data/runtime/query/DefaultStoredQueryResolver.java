250a2e198ca81dc4f8c7f64b2615f88db6422afa
Denis Stepanov <denis.s.stepanov@oracle.com>
2024-09-05 13:16:21 +0200

    Fix mongo custom query pagination (#3106)

--- data-runtime/src/main/java/io/micronaut/data/runtime/query/DefaultStoredQueryResolver.java
+++ data-runtime/src/main/java/io/micronaut/data/runtime/query/DefaultStoredQueryResolver.java
@@ -66,9 +66,9 @@ public abstract class DefaultStoredQueryResolver implements StoredQueryResolver 
 
     @Override
     public <E, R> StoredQuery<E, R> resolveCountQuery(MethodInvocationContext<?, ?> context, Class<E> entityClass, Class<R> resultType) {
-        String query = context.stringValue(Query.class, DataMethod.META_MEMBER_COUNT_QUERY).orElseThrow(() ->
-                new IllegalStateException("No query present in method")
-        );
+        String query = context.stringValue(Query.class, DataMethod.META_MEMBER_COUNT_QUERY)
+            .orElseGet(() -> context.stringValue(Query.class)
+                .orElseThrow(() -> new IllegalStateException("No query present in method")));
         return new DefaultStoredQuery<>(
                 context.getExecutableMethod(),
                 resultType,
