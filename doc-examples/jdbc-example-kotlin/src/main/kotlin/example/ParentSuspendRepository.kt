afe35b88637123a41345e6b47764093c8ff56130
radovanradic <radicr@gmail.com>
2025-02-27 17:16:29 +0100

    Fix more tests, rethink approach

--- doc-examples/jdbc-example-kotlin/src/main/kotlin/example/ParentSuspendRepository.kt
+++ doc-examples/jdbc-example-kotlin/src/main/kotlin/example/ParentSuspendRepository.kt
@@ -3,8 +3,11 @@ package example
 import io.micronaut.core.annotation.NonNull
 import io.micronaut.data.annotation.Join
 import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.GenericRepository
+import io.micronaut.data.repository.jpa.criteria.QuerySpecification
 import io.micronaut.data.repository.jpa.kotlin.CoroutineJpaSpecificationExecutor
 import io.micronaut.data.runtime.criteria.get
 import io.micronaut.data.runtime.criteria.joinMany
@@ -19,6 +22,9 @@ interface ParentSuspendRepository : GenericRepository<Parent, Int>, CoroutineJpa
 
     @Join(value = "children", type = Join.Type.FETCH)
     suspend fun findById(id: Int): Optional<Parent>
+
+    @Join(value = "children", type = Join.Type.FETCH)
+    override suspend fun findAll(spec: QuerySpecification<Parent>?, pageable: Pageable): Page<Parent>
 
     @Transactional(Transactional.TxType.MANDATORY)
     suspend fun queryById(id: Int): Optional<Parent>
