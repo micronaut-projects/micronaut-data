a4dfc9658b80326277edfca9b616e4792a927b81
radovanradic <radicr@gmail.com>
2025-08-18 09:28:48 +0200

    Fix resolving mongodb (aggregate) query when it has leading whitespaces

--- data-mongodb/src/main/java/io/micronaut/data/mongodb/operations/DefaultMongoStoredQuery.java
+++ data-mongodb/src/main/java/io/micronaut/data/mongodb/operations/DefaultMongoStoredQuery.java
@@ -133,7 +133,7 @@ final class DefaultMongoStoredQuery<E, R> extends DefaultBindableParametersStore
         this.persistentEntity = persistentEntity;
         OperationType operationType = storedQuery.getOperationType();
         if (operationType == OperationType.QUERY || operationType == OperationType.EXISTS || operationType == OperationType.COUNT) {
-            String query = storedQuery.getQuery();
+            String query = StringUtils.trimLeadingWhitespace(storedQuery.getQuery());
             String filterParameter = getParameterInRole(MongoRoles.FILTER_ROLE);
             String filterOptionsParameter = getParameterInRole(MongoRoles.FIND_OPTIONS_ROLE);
             String pipelineParameter = getParameterInRole(MongoRoles.PIPELINE_ROLE);
