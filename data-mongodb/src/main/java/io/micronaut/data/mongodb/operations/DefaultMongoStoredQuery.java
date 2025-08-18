f68630675019406d09672f918e2cb6d4ca4cd41b
radovanradic <radicr@gmail.com>
2025-08-18 10:19:44 +0200

    Change pe CR suggestion

--- data-mongodb/src/main/java/io/micronaut/data/mongodb/operations/DefaultMongoStoredQuery.java
+++ data-mongodb/src/main/java/io/micronaut/data/mongodb/operations/DefaultMongoStoredQuery.java
@@ -133,7 +133,10 @@ final class DefaultMongoStoredQuery<E, R> extends DefaultBindableParametersStore
         this.persistentEntity = persistentEntity;
         OperationType operationType = storedQuery.getOperationType();
         if (operationType == OperationType.QUERY || operationType == OperationType.EXISTS || operationType == OperationType.COUNT) {
-            String query = StringUtils.trimLeadingWhitespace(storedQuery.getQuery());
+            String query = storedQuery.getQuery();
+            if (query != null) {
+                query = query.trim();
+            }
             String filterParameter = getParameterInRole(MongoRoles.FILTER_ROLE);
             String filterOptionsParameter = getParameterInRole(MongoRoles.FIND_OPTIONS_ROLE);
             String pipelineParameter = getParameterInRole(MongoRoles.PIPELINE_ROLE);
