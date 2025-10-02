cce61b4960c39c3ebf7a436d61de6baaf6ee902f
radovanradic <radicr@gmail.com>
2025-10-02 20:55:09 +0200

    Added test to verify fix does not make regression like previous PR

---
+++ doc-examples/jdbc-example-kotlin/src/main/kotlin/example/ClientRepository.kt
@@ -0,0 +1,10 @@
+package example
+
+import io.micronaut.data.annotation.Join
+import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.repository.CrudRepository
+
+@JdbcRepository(dialect = Dialect.H2)
+@Join(value = "relationship.status", type = Join.Type.LEFT_FETCH)
+interface ClientRepository : CrudRepository<Client, Long>
---
+++ doc-examples/jdbc-example-kotlin/src/test/resources/client.sql
@@ -0,0 +1,21 @@
+drop table relationship_status if exists cascade;
+drop table client if exists cascade;
+
+create table relationship_status
+(
+    id   bigserial primary key,
+    name text not null
+);
+
+insert into relationship_status (id, name) values (1, 'Active');
+insert into relationship_status (id, name) values (2, 'Moved to better place');
+
+create table client
+(
+    id                     bigserial primary key,
+    name                   text      not null,
+    relationship_type      text      not null,
+    relationship_status_id bigint    not null references relationship_status (id),
+    created_at             timestamp not null default now(),
+    updated_at             timestamp not null default now()
+);
