e4717184c899d2fccc612699e3eafb2835954a03
radovanradic <radicr@gmail.com>
2025-11-03 16:24:29 +0100

    Reproducer for self reference update issue

---
+++ doc-examples/jdbc-example-kotlin/src/main/kotlin/example/BarRepository.kt
@@ -0,0 +1,8 @@
+package example
+
+import io.micronaut.data.jdbc.annotation.JdbcRepository
+import io.micronaut.data.model.query.builder.sql.Dialect
+import io.micronaut.data.repository.CrudRepository
+
+@JdbcRepository(dialect = Dialect.H2)
+abstract class BarRepository : CrudRepository<Bar, Long>
