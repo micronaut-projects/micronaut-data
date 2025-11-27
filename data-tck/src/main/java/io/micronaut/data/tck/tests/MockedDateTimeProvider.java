60c920f22a2bee33ef8c9d0bbe6f17f4d7d5b535
Denis Stepanov <denis.s.stepanov@oracle.com>
2025-11-27 13:43:53 +0100

    Jakarta Data 1.1 M1 Support stateful annotations

---
+++ data-tck/src/main/java/io/micronaut/data/tck/tests/MockedDateTimeProvider.java
@@ -0,0 +1,28 @@
+package io.micronaut.data.tck.tests;
+
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.data.runtime.date.DateTimeProvider;
+import jakarta.inject.Singleton;
+
+import java.time.OffsetDateTime;
+
+@Singleton
+@Replaces(DateTimeProvider.class)
+public class MockedDateTimeProvider implements DateTimeProvider<OffsetDateTime> {
+
+    private OffsetDateTime value;
+
+    public void setValue(OffsetDateTime value) {
+        this.value = value;
+    }
+
+    @Override
+    public OffsetDateTime getNow() {
+        if (value == null) {
+            return OffsetDateTime.now();
+        }
+        OffsetDateTime currentValue = value;
+        value = null;
+        return currentValue;
+    }
+}
