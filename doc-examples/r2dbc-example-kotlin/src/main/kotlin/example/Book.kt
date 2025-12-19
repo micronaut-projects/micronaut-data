095e0e18d5c2f382958a36520a74062c341d0c1a
radovanradic <radicr@gmail.com>
2025-12-19 17:02:44 +0100

    Investigate cursored pageable issue

--- doc-examples/r2dbc-example-kotlin/src/main/kotlin/example/Book.kt
+++ doc-examples/r2dbc-example-kotlin/src/main/kotlin/example/Book.kt
@@ -5,7 +5,6 @@ import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
 import io.micronaut.data.annotation.Relation
 import io.micronaut.serde.annotation.Serdeable
-import org.jspecify.annotations.Nullable
 
 @Serdeable
 @MappedEntity
