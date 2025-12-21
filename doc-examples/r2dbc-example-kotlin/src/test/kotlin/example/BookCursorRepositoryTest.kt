98fe4cf90c174172f9b3c6d820d20021c017338f
radovanradic <radicr@gmail.com>
2025-12-21 19:35:44 +0100

    Investigate cursored pageable issue

--- doc-examples/r2dbc-example-kotlin/src/test/kotlin/example/BookCursorRepositoryTest.kt
+++ doc-examples/r2dbc-example-kotlin/src/test/kotlin/example/BookCursorRepositoryTest.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
@@ -25,6 +26,20 @@ class BookCursorRepositoryTest : AbstractTest(false) {
     @Inject
     lateinit var blockingBookRepository: BlockingBookRepository
 
+    @BeforeEach
+    fun setup() {
+        val author = Author("Some")
+        blockingAuthorRepository.save(author)
+        val titles = listOf(
+            "A Tale of Two Cities", "Brave New World", "Catch-22",
+            "Dune", "Ender's Game", "Fahrenheit 451", "Gone Girl", "Hyperion"
+        )
+        // This will produce 6 books with pages >= 400 to be verified in the tests
+        titles.forEachIndexed { i, t ->
+            blockingBookRepository.save(Book(t, 300 + (i * 50), author))
+        }
+    }
+
     @AfterEach
     fun cleanup() {
         blockingBookRepository.deleteAll()
@@ -32,19 +47,7 @@ class BookCursorRepositoryTest : AbstractTest(false) {
     }
 
     @Test
-    fun `suspend fun + CursoredPage with @Query and countQuery does not block and paginates`() = runBlocking {
-        val author = Author("Some")
-        blockingAuthorRepository.save(author)
-        val titles = listOf(
-            "A Tale of Two Cities", "Brave New World", "Catch-22",
-            "Dune", "Ender's Game", "Fahrenheit 451", "Gone Girl", "Hyperion"
-        )
-        // This will produce 6 books with pages >= 400 to be verifed in the tests
-        titles.forEachIndexed { i, t ->
-            blockingBookRepository.save(Book(t, 300 + (i * 50), author))
-        }
-
-        // First page (requestTotal=true by default for CursoredPageable.from)
+    fun testCursoredPageableWithCountDoesNotBlock() = runBlocking {
         val pageable = CursoredPageable.from(3, Sort.unsorted())
         val page1 = bookCursorRepository.findByCursor(400, pageable)
         assertTrue(page1.content.isNotEmpty())
@@ -52,14 +55,24 @@ class BookCursorRepositoryTest : AbstractTest(false) {
         assertEquals(6, page1.totalSize)
         assertTrue(page1.hasTotalSize())
 
-        // Next cursored page
         val page2 = bookCursorRepository.findByCursor(400, page1.nextPageable())
         assertTrue(page2.numberOfElements > 0)
         assertEquals(6, page1.totalSize)
 
-        // Regular pageable + count
         val offsetPage = bookCursorRepository.findByCursor(400, Pageable.from(0, 2))
         assertEquals(2, offsetPage.numberOfElements)
         assertEquals(6, offsetPage.totalSize)
+    }
+
+    @Test
+    fun testCursoredPageWithoutTotal() = runBlocking {
+        val pageable = CursoredPageable.from(3, Sort.unsorted()).withoutTotal()
+        val page1 = bookCursorRepository.findByCursor(400, pageable)
+
+        assertTrue(page1.content.isNotEmpty())
+        assertEquals(3, page1.numberOfElements)
+
+        val page2 = bookCursorRepository.findByCursor(400, page1.nextPageable())
+        assertTrue(page2.numberOfElements > 0)
     }
 }
