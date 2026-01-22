0adca8b62bfd28a5607d8eab88d378e0707d1168
radovanradic <radicr@gmail.com>
2026-01-22 12:42:43 +0100

    Support snake case repository methods naming

--- data-processor/src/test/groovy/io/micronaut/data/processor/visitors/SnakeCaseFindersSpec.groovy
+++ data-processor/src/test/groovy/io/micronaut/data/processor/visitors/SnakeCaseFindersSpec.groovy
@@ -16,6 +16,7 @@
 package io.micronaut.data.processor.visitors
 
 import spock.lang.Unroll
+import static io.micronaut.data.processor.visitors.TestUtils.getQuery
 
 class SnakeCaseFindersSpec extends AbstractDataSpec {
 
@@ -23,28 +24,33 @@ class SnakeCaseFindersSpec extends AbstractDataSpec {
         given:
         def repository = buildRepository('test.BookRepository', """
 import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.GenericRepository;
 import io.micronaut.data.tck.entities.Book;
 
 @Repository
-interface BookRepository extends CrudRepository<Book, Long> {
+interface BookRepository extends GenericRepository<Book, Long> {
 
     Book find_by_title(String title);
 }
 """)
-        expect:
-        repository.getRequiredMethod("find_by_title", String) != null
+        when:
+        def method = repository.getRequiredMethod("find_by_title", String)
+        def query = getQuery(method)
+        then:
+        query == 'SELECT book_ FROM io.micronaut.data.tck.entities.Book AS book_ WHERE (book_.title = :p1)'
+
     }
 
     void "test count_distinct_by_name"() {
         given:
         def repository = buildRepository('test.PersonRepository', """
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.model.entities.Person;
+import io.micronaut.data.repository.GenericRepository;
 
-@Repository
-interface PersonRepository extends CrudRepository<Person, Long> {
+@JdbcRepository(dialect = Dialect.MYSQL)
+interface PersonRepository extends GenericRepository<Person, Long> {
 
     long count_distinct_by_name(String name);
 }
@@ -53,38 +59,46 @@ interface PersonRepository extends CrudRepository<Person, Long> {
         def method = repository.getRequiredMethod("count_distinct_by_name", String)
         then:
         method != null
+        when:
+        def query = getQuery(method)
+        then:
+        query == 'SELECT COUNT(DISTINCT(person_.`id`)) FROM `person` person_ WHERE (person_.`name` = ?)'
     }
 
     void "test delete_by_id_returning compiles"() {
         given:
         def repository = buildRepository('test.BookRepository', """
-import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.repository.GenericRepository;
 import io.micronaut.data.tck.entities.Book;
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
-interface BookRepository extends CrudRepository<Book, Long> {
+interface BookRepository extends GenericRepository<Book, Long> {
 
     int delete_by_id_returning(Long id);
 }
 """)
-        expect:
-        repository.getRequiredMethod("delete_by_id_returning", Long) != null
+        when:
+        def method = repository.getRequiredMethod("delete_by_id_returning", Long)
+        def query = getQuery(method)
+        then:
+        query == 'DELETE  FROM "book"  WHERE ("id" = ?) RETURNING "id","author_id","genre_id","title","total_pages","publisher_id","last_updated"'
     }
 
     @Unroll
     void "test complex snake_case '#name' parses"() {
         given:
         def repository = buildRepository('test.BookRepository', """
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
+import io.micronaut.data.repository.GenericRepository;
 import io.micronaut.data.tck.entities.Book;
 import io.micronaut.data.tck.entities.Author;
 
-@Repository
-interface BookRepository extends CrudRepository<Book, Long> {
+@JdbcRepository(dialect = Dialect.H2)
+interface BookRepository extends GenericRepository<Book, Long> {
 
     java.util.List<Book> ${name}(String a, String b);
 }
@@ -94,6 +108,7 @@ interface BookRepository extends CrudRepository<Book, Long> {
         where:
         name << [
             'find_all_by_author_name_or_title_like_order_by_total_pages_desc',
+            'query_all_by_title_or_author_name'
         ]
     }
 
