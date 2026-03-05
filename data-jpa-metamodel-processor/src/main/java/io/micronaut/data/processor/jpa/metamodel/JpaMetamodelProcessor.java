c268b4e8bff702cc0593904f254640ba229a66e1
Mohammed Chbani <mohammed.chbani@oracle.com>
2026-03-05 10:44:14 +0000

    Jpa metamodel processor added with support for inheritance and transient fields

---
+++ data-jpa-metamodel-processor/src/main/java/io/micronaut/data/processor/jpa/metamodel/JpaMetamodelProcessor.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.data.processor.jpa.metamodel;
+
+import io.micronaut.core.naming.NameUtils;
+import io.micronaut.inject.ast.ClassElement;
+import io.micronaut.inject.ast.PropertyElement;
+import io.micronaut.sourcegen.model.*;
+import org.jspecify.annotations.NonNull;
+
+import javax.lang.model.element.Modifier;
+import java.util.*;
+
+/**
+ * Jpa Static Metamodel processor.
+ */
+public final class JpaMetamodelProcessor {
+
+    /**
+     * Supported Jakarta annotations for generating Static meta model classes.
+     */
+    public static final Set<String> SUPPORTED_JAKARTA_ANNOTATIONS = new HashSet<>(Arrays.asList("jakarta.persistence.Entity",
+        "jakarta.persistence.MappedSuperclass",
+        "jakarta.persistence.Embeddable"));
+
+    /**
+     * Default constructor.
+     */
+    public JpaMetamodelProcessor() {
+    }
+
+    /**
+     * JPA meta model class def generator .
+     *
+     * @param packageName          Element package name
+     * @param elementType          Element type
+     * @param optionalSuperElement Element super type
+     * @param properties           element properties/fields
+     * @return Jpa metamodel class definition builder .
+     */
+    public static ClassDef.ClassDefBuilder createJpaMetaModelClassDefBuilder(@NonNull String packageName, @NonNull ClassTypeDef elementType, Optional<ClassElement> optionalSuperElement, List<PropertyElement> properties) {
+        String metaModelClassName = resolveModelClassName(packageName, elementType);
+
+        ClassDef.ClassDefBuilder classDefBuilder = ClassDef.builder(metaModelClassName)
+            .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+            .addAnnotation(AnnotationDef.builder(ClassTypeDef.of("jakarta.persistence.metamodel.StaticMetamodel")).addMember("value", elementType).build())
+            .addAnnotation(AnnotationDef.builder(ClassTypeDef.of("jakarta.annotation.Generated")).addMember("value", JpaMetamodelProcessor.class.getName()).build());
+
+        ClassElement superElement = optionalSuperElement.orElse(null);
+
+        if (superElement != null && supportedClass(superElement)) {
+            String superElementModelClassName = resolveModelClassName(superElement.getPackageName(), ClassTypeDef.of(superElement));
+            ClassTypeDef superClassModelTypeDef = ClassTypeDef.of(superElementModelClassName);
+            classDefBuilder.superclass(superClassModelTypeDef);
+        }
+
+        properties = properties.stream().filter(o -> !o.getAnnotationNames().contains("jakarta.persistence.Transient"))
+            .filter(o -> o.getDeclaringType().getName().equals(elementType.getName()))
+            .toList();
+
+        List<FieldDef> constantPropertyName = new ArrayList<>();
+        List<FieldDef> attributeFields = new ArrayList<>();
+
+        for (PropertyElement beanProperty : properties) {
+            constantPropertyName.add(createConstantPropertyName(beanProperty));
+            attributeFields.add(createAttributeField(beanProperty, elementType));
+        }
+
+        classDefBuilder.addFields(constantPropertyName);
+        classDefBuilder.addFields(attributeFields);
+        classDefBuilder.addField(createEntityTypeField(elementType));
+        classDefBuilder.addMethod(MethodDef.constructor().build());
+        return classDefBuilder;
+    }
+
+    /**
+     *
+     * @param packageName package name
+     * @param elementType element type
+     * @return static metamodel class canonical name_ .
+     */
+    private static String resolveModelClassName(String packageName, ClassTypeDef elementType) {
+        String localBinaryName = elementType.getName().startsWith(packageName + ".") ? elementType.getName().substring(packageName.isEmpty() ? 0 : packageName.length() + 1) : elementType.getName();
+        String baseName = elementType.isInner() ? localBinaryName.replace("$", "") : elementType.getSimpleName();
+        String metaModelClassSimpleName = baseName + "_";
+        return packageName + "." + metaModelClassSimpleName;
+    }
+
+    /**
+     * Utility function to check if the given class is supported for StaticMetamodel generation.
+     *
+     * @param classElement class element
+     * @return boolean
+     */
+    public static boolean supportedClass(ClassElement classElement) {
+        return !classElement.isInner() && classElement.getAnnotationNames().stream().anyMatch(SUPPORTED_JAKARTA_ANNOTATIONS::contains);
+    }
+
+    /**
+     * @param elementType class type definition
+     * @return FieldDef
+     */
+    private static FieldDef createEntityTypeField(ClassTypeDef elementType) {
+        return FieldDef.builder("class_").addModifiers(Modifier.PUBLIC, Modifier.VOLATILE, Modifier.STATIC)
+            .ofType(TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.EntityType"), elementType)).build();
+    }
+
+    /**
+     * @param beanProperty field
+     * @return FieldDef
+     */
+    private static FieldDef createConstantPropertyName(PropertyElement beanProperty) {
+        return FieldDef.builder(NameUtils.underscoreSeparate(beanProperty.getName()).toUpperCase(Locale.ROOT))
+            .ofType(TypeDef.STRING)
+            .addModifiers(Modifier.PUBLIC, Modifier.FINAL, Modifier.STATIC)
+            .initializer(ExpressionDef.constant(beanProperty.getSimpleName())).build();
+    }
+
+    /**
+     * Create attribute fields SingularAttribute,ListAttribute... based on the element type .
+     *
+     * @param beanProperty Field
+     * @param classTypeDef Field type
+     * @return FieldDef
+     */
+    private static FieldDef createAttributeField(PropertyElement beanProperty, ClassTypeDef classTypeDef) {
+        FieldDef.FieldDefBuilder attributeDefBuilder = FieldDef.builder(beanProperty.getName()).addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.VOLATILE);
+
+        TypeDef typeDef = switch (beanProperty.getType().getName()) {
+            case "java.util.Collection" ->
+                TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.CollectionAttribute"), classTypeDef, TypeDef.of(beanProperty.getType().getTypeArguments().get("E")));
+            case "java.util.Set" ->
+                TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.SetAttribute"), classTypeDef, TypeDef.of(beanProperty.getType().getTypeArguments().get("E")));
+            case "java.util.List" ->
+                TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.ListAttribute"), classTypeDef, TypeDef.of(beanProperty.getType().getTypeArguments().get("E")));
+            case "java.util.Map" ->
+                TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.MapAttribute"), classTypeDef, TypeDef.of(beanProperty.getType().getTypeArguments().get("K")), TypeDef.of(beanProperty.getType().getTypeArguments().get("V")));
+            default ->
+                TypeDef.parameterized(ClassTypeDef.of("jakarta.persistence.metamodel.SingularAttribute"), classTypeDef, getProperType(TypeDef.of(beanProperty.getType())));
+        };
+        return attributeDefBuilder.ofType(typeDef).build();
+    }
+
+    /**
+     *
+     * @param type
+     * @return
+     */
+    private static TypeDef getProperType(TypeDef type) {
+        if (type.isPrimitive() && type instanceof TypeDef.Primitive primitive) {
+            return TypeDef.of(primitive.wrapperType().getName());
+        }
+        return type;
+    }
+}
---
+++ data-jpa-metamodel-processor/src/main/java/io/micronaut/data/processor/jpa/metamodel/visitor/JpaMetamodelProcessorVisitor.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.data.processor.jpa.metamodel.visitor;
+
+
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.processor.jpa.metamodel.JpaMetamodelProcessor;
+import io.micronaut.inject.ast.ClassElement;
+import io.micronaut.inject.ast.PropertyElement;
+import io.micronaut.inject.processing.ProcessingException;
+import io.micronaut.inject.visitor.TypeElementVisitor;
+import io.micronaut.inject.visitor.VisitorContext;
+import io.micronaut.sourcegen.generator.SourceGenerator;
+import io.micronaut.sourcegen.generator.SourceGenerators;
+import io.micronaut.sourcegen.model.ClassDef;
+import io.micronaut.sourcegen.model.ClassTypeDef;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/** Jpa static meta model annotation processor visitor.
+ *
+ */
+@Internal
+public final class JpaMetamodelProcessorVisitor implements TypeElementVisitor<Object, Object> {
+
+    private final Set<String> processed = new HashSet<>();
+
+    /**
+     * Default constructor.
+     */
+    public JpaMetamodelProcessorVisitor() {
+    }
+
+    /**
+     *
+     * @return Set of string of supported annotation names
+     */
+    @Override
+    public Set<String> getSupportedAnnotationNames() {
+        return JpaMetamodelProcessor.SUPPORTED_JAKARTA_ANNOTATIONS;
+    }
+
+    /**
+     * @param element class element
+     * @param context visitor context
+     */
+    @Override
+    public void visitClass(ClassElement element, VisitorContext context) {
+        if (!JpaMetamodelProcessor.supportedClass(element) ||
+            processed.contains(element.getName())) {
+            return;
+        }
+        try {
+            List<PropertyElement> properties = element.getBeanProperties();
+            ClassTypeDef elementType = ClassTypeDef.of(element);
+            ClassDef.ClassDefBuilder builder = JpaMetamodelProcessor.createJpaMetaModelClassDefBuilder(element.getPackageName(), elementType, element.getSuperType(), properties);
+            ClassDef builderDef = builder.build();
+            SourceGenerator sourceGenerator = SourceGenerators.findByLanguage(context.getLanguage()).orElse(null);
+            if (sourceGenerator == null) {
+                return;
+            }
+            processed.add(element.getName());
+            sourceGenerator.write(builderDef, context, element);
+        } catch (ProcessingException e) {
+            throw e;
+        } catch (Exception e) {
+            String message = e.getMessage() != null ? e.getMessage() : e.getClass().getSimpleName();
+            throw new ProcessingException(element, "Failed to generate a @" + "jakarta.persistence.metamodel.StaticMetamodel" + ": " + message, e);
+        }
+    }
+
+    /**
+     * @param visitorContext visitor context
+     */
+    @Override
+    public void start(VisitorContext visitorContext) {
+        this.processed.clear();
+    }
+
+    /**
+     * @return Visitor kind
+     */
+    @Override
+    public @NonNull VisitorKind getVisitorKind() {
+        return VisitorKind.ISOLATING;
+    }
+
+}
