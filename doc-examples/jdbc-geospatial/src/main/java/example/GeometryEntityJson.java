003f3136d2c9fcd1a2bcaa172839d92a03ff1dc2
Milenko Supic <milenko.supic@oracle.com>
2026-03-31 15:13:10 +0200

    GeoSpatial - added doc-examples:jdbc-geospatial

---
+++ doc-examples/jdbc-geospatial/src/main/java/example/GeometryEntityJson.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.data.annotation.GeneratedValue;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.Index;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.MappedProperty;
+import io.micronaut.data.annotation.Srid;
+import io.micronaut.data.model.geo.MultiPoint;
+import io.micronaut.data.model.geo.Point;
+
+@MappedEntity
+public class GeometryEntityJson {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Srid(3857)
+    @Index(columns = "location")
+    @MappedProperty("location")
+    private Point point;
+
+    @Nullable
+    private MultiPoint multiPoint;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public MultiPoint getMultiPoint() {
+        return multiPoint;
+    }
+
+    public void setMultiPoint(MultiPoint multiPoint) {
+        this.multiPoint = multiPoint;
+    }
+}
