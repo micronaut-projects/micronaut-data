afd7a4875057e38e8d44806f2e9ded848d7532da
radovanradic <radicr@gmail.com>
2026-04-14 14:53:40 +0200

    Revert async interceptor changes.

--- data-runtime/src/main/java/io/micronaut/data/runtime/intercept/async/DefaultUpdateReturningOneAsyncInterceptor.java
+++ data-runtime/src/main/java/io/micronaut/data/runtime/intercept/async/DefaultUpdateReturningOneAsyncInterceptor.java
@@ -47,7 +47,11 @@ public final class DefaultUpdateReturningOneAsyncInterceptor<T, R> extends Abstr
     @Override
     public CompletionStage<R> intercept(RepositoryMethodKey methodKey, MethodInvocationContext<T, CompletionStage<R>> context) {
         PreparedQuery<?, R> preparedQuery = prepareQuery(methodKey, context);
-        return asyncDatastoreOperations.execute(preparedQuery)
-            .thenApply(ts -> ts.isEmpty() ? (R) convertOne(context, (Object) null) : ts.iterator().next());
+        return asyncDatastoreOperations.execute(preparedQuery).thenApply(ts -> {
+            if (ts.isEmpty()) {
+                return null;
+            }
+            return ts.iterator().next();
+        });
     }
 }
