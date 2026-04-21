ee7004449e4a2aa65d58c861ceb274b39d49cd7b
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-21 16:23:14 +0200

    Update to latest (#3816)
    * micronaut-platform = "5.0.0-M1"
    
    * micronaut-spring = "6.0.0-M1"
    
    * micronaut-test = "5.0.0-M8"
    
    * micronaut-kotlin = "5.0.0-M1"
    
    * fix nullaway errors
    
    * Kotest 6 changed AbstractProjectConfig from overridable functions to properties.
    
    * use kotlinx.coroutines.reactor defined in core
    
    * use kotlinx.coroutines.reactive defined in core
    
    * use .kotlinx.coroutines.core defined in core
    
    * add kotlin.coroutines.test
    
    * git: ignore .kotlin
    
    * Fix issues with duplicate @Property on tests and nullability with spring transactions.
    
    ---------
    
    Co-authored-by: radovanradic <radicr@gmail.com>

--- doc-examples/hibernate-example-kotlin-ksp/src/test/kotlin/example/ProjectConfig.kt
+++ doc-examples/hibernate-example-kotlin-ksp/src/test/kotlin/example/ProjectConfig.kt
@@ -4,6 +4,5 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 
 object ProjectConfig : AbstractProjectConfig() {
-    override fun listeners() = listOf(MicronautKotest5Extension)
-    override fun extensions() = listOf(MicronautKotest5Extension)
+    override val extensions = listOf(MicronautKotest5Extension)
 }
