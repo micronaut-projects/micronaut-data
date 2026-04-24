4c48c81216909cf4178e277943476129d6f5e368
Sergio del Amo <sergio.delamo@softamo.com>
2026-04-24 15:56:25 +0200

    batch insert

--- data-jdbc/src/main/java/io/micronaut/data/jdbc/operations/DefaultJdbcRepositoryOperations.java
+++ data-jdbc/src/main/java/io/micronaut/data/jdbc/operations/DefaultJdbcRepositoryOperations.java
@@ -1176,7 +1176,7 @@ public final class DefaultJdbcRepositoryOperations extends AbstractSqlRepository
     public boolean isSupportsBatchInsert(JdbcOperationContext jdbcOperationContext, RuntimePersistentEntity<?> persistentEntity) {
         if (persistentEntity.hasIdentity()
             && persistentEntity.getIdentity().isGenerated()
-            && !ConnectionCapabilities.INSTANCE.supports(ConnectionCapabilities.Capability.BATCH_INSERT, jdbcOperationContext.connection)) {
+            && !jdbcOperationContext.dialect.supportsBatchInsert()) {
             return false;
         }
         return isSupportsBatchInsert(persistentEntity, jdbcOperationContext.dialect);
