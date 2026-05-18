732d07a777b78e1da6740062ea1ea72e08360a6c
radovanradic <radicr@gmail.com>
2026-05-18 14:04:40 +0200

    Add missing import

--- data-processor/src/main/java/io/micronaut/data/processor/model/SourcePersistentEntity.java
+++ data-processor/src/main/java/io/micronaut/data/processor/model/SourcePersistentEntity.java
@@ -15,6 +15,7 @@
  */
 package io.micronaut.data.processor.model;
 
+import io.micronaut.data.annotation.JsonSubView;
 import org.jspecify.annotations.Nullable;
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.core.util.ArrayUtils;
