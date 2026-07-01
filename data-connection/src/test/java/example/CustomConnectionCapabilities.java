d242cfbab6d482341ccf2649875b38314134ea0c
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-07-01 14:27:36 +0200

    revert changes

--- data-connection/src/test/java/example/CustomConnectionCapabilities.java
+++ data-connection/src/test/java/example/CustomConnectionCapabilities.java
@@ -3,11 +3,10 @@ package example;
 import io.micronaut.data.connection.ConnectionCapabilities;
 
 import java.sql.Connection;
-import java.util.function.Supplier;
 
 public class CustomConnectionCapabilities implements ConnectionCapabilities {
     @Override
-    public boolean supports(Capability capability, Connection connection) {
+    public boolean supports(ConnectionCapabilities.Capability capability, Connection connection) {
         return true;
     }
 }
