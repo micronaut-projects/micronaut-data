683734354ec769a7969a0f7aa707521adc257395
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-07-01 14:48:57 +0200

    update select id

--- test-suite-data-jdbc-sqlite/src/test/java/io/micronaut/data/jdbc/sqlite/identity/MyBookRepository.java
+++ test-suite-data-jdbc-sqlite/src/test/java/io/micronaut/data/jdbc/sqlite/identity/MyBookRepository.java
@@ -12,21 +12,21 @@ public interface MyBookRepository extends CrudRepository<MyBook, Integer> {
     @Query("""
         SELECT 1 AS id, 'Title #1' AS title
         UNION ALL
-        SELECT 1 AS id, 'Title #2' AS title
+        SELECT 2 AS id, 'Title #2' AS title
         """)
     List<MyBook> getBooks();
 
     @Query("""
         SELECT 1 AS id, 'Title #1' AS title
         UNION ALL
-        SELECT 1 AS id, 'Title #2' AS title
+        SELECT 2 AS id, 'Title #2' AS title
         """)
     List<MyBookDto> getBooksAsDto();
 
     @Query("""
         SELECT 1 AS id_renamed, 'Title #1' AS title
         UNION ALL
-        SELECT 1 AS id_renamed, 'Title #2' AS title
+        SELECT 2 AS id_renamed, 'Title #2' AS title
         """)
     List<MyBookDto2> getBooksAsDto2();
 }
