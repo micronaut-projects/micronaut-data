0a428b40356aba09e294c6c5ada5c8d072558f83
Milenko Supic <milenko.supic@oracle.com>
2026-08-11 17:19:29 +0200

    Query Notification implementation - cleanup

--- data-jdbc/src/test/groovy/io/micronaut/data/jdbc/oraclexe/notification/ObjectChangeNotificationBookRepository.groovy
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/oraclexe/notification/ObjectChangeNotificationBookRepository.groovy
@@ -7,8 +7,6 @@ import io.micronaut.data.repository.CrudRepository
 
 @JdbcRepository(dialect = Dialect.ORACLE)
 interface ObjectChangeNotificationBookRepository extends CrudRepository<ObjectChangeNotificationBook, Long> {
-    @Query(value = "SELECT ROWID FROM object_change_notification_book WHERE id = :id", nativeQuery = true)
-    Optional<String> findRowIdById(Long id)
 
     @Query("UPDATE object_change_notification_book SET title = :title WHERE id IN (:ids)")
     long updateTitleByIds(String title, List<Long> ids)
