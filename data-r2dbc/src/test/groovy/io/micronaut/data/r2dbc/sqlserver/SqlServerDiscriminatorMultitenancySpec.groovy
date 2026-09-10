abc77bacff2ee2538b78cd1f123e706771cc430a
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-09-10 07:46:48 +0200

    Temporarily disable SQL Server R2DBC multitenancy tests
    Mark the SQL Server discriminator multitenancy specification as ignored because the SQL Server R2DBC tests are temporarily disabled.

--- data-r2dbc/src/test/groovy/io/micronaut/data/r2dbc/sqlserver/SqlServerDiscriminatorMultitenancySpec.groovy
+++ data-r2dbc/src/test/groovy/io/micronaut/data/r2dbc/sqlserver/SqlServerDiscriminatorMultitenancySpec.groovy
@@ -2,7 +2,9 @@ package io.micronaut.data.r2dbc.sqlserver
 
 
 import io.micronaut.data.tck.tests.AbstractDiscriminatorMultitenancySpec
+import spock.lang.Ignore
 
+@Ignore("SQL Server R2DBC tests are temporarily disabled")
 class SqlServerDiscriminatorMultitenancySpec extends AbstractDiscriminatorMultitenancySpec implements SqlServerTestPropertyProvider {
 
     @Override
