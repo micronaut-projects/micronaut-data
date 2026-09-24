a5524478135801d26ef020f20e8c9cfc00e99f50
radovanradic <radicr@gmail.com>
2026-09-24 19:35:16 +0200

    Handle read-only transaction errors in MariaDB tests

--- data-jdbc/src/test/groovy/io/micronaut/data/jdbc/mariadb/MariaTransactionsSpec.groovy
+++ data-jdbc/src/test/groovy/io/micronaut/data/jdbc/mariadb/MariaTransactionsSpec.groovy
@@ -11,4 +11,16 @@ class MariaTransactionsSpec extends AbstractJdbcTransactionSpec implements Maria
         return MySqlBookRepository.class
     }
 
+    @Override
+    boolean failsInsertInReadOnlyTx() {
+        // Enforced since MariaDB Connector/J 3.5.10
+        return true
+    }
+
+    @Override
+    boolean cannotInsertInReadOnlyTx(Exception e) {
+        assert e.cause.message.endsWith("Cannot execute statement in a READ ONLY transaction")
+        return true
+    }
+
 }
